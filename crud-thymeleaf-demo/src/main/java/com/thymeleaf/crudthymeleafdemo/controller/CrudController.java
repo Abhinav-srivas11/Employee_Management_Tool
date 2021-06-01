2e3b888e6567b1b2de90ec47d6b8f21080298652
Abhinav Srivastava <abhinavsrivastava54@yahoo.com>
2021-06-01 18:55:02 +0530

    Pagination Added
    Added pagination using PagingAndSortingRepository<> and Thymeleaf

--- crud-thymeleaf-demo/src/main/java/com/thymeleaf/crudthymeleafdemo/controller/CrudController.java
+++ crud-thymeleaf-demo/src/main/java/com/thymeleaf/crudthymeleafdemo/controller/CrudController.java
@@ -1,5 +1,7 @@
 package com.thymeleaf.crudthymeleafdemo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,15 +22,39 @@ public class CrudController {
 	@Autowired
 	private EmployeeService employeeService;
 
-	@GetMapping("/employees")
-	public String tableOfEmployees(Model model) {
-		model.addAttribute("employees", employeeService.findAllEmployees());
+//	@GetMapping("/employees")
+//	public String tableOfEmployees(Model model) {
+//		model.addAttribute("employees", employeeService.findAllEmployees());
+//
+//		return "employee-page";
+//	}
 
+	// Alternate way that supports paging
+	@GetMapping("/employees")
+	// we have passed default values to our page number and page size integers so
+	// that we do not leave it null and cause errors
+	public String tableOfEmployees(Model model, @RequestParam(defaultValue = "0") Integer pageNo,
+			@RequestParam(defaultValue = "10") Integer pageSize) {
+		// we have defined the method in service layer that intakes pageNo and pageSize
+		// parameters to create a Pageable object
+		List<Employee> listEmployees = employeeService.findAllWithPage(pageNo, pageSize);
+		// adding the list of employees to our employees attribute in the model
+		model.addAttribute("employees", listEmployees);
+		// since we need to work with page numbers for navigation purposes
+		// i have added the single attribute to model making it easier to access values
+		model.addAttribute("page", pageNo);
+		// calculating the max number of pages that are allowed
+		Integer numOfPages = (listEmployees.size() / pageSize) + 1;
+		// adding it to second attribute in our model
+		// second attribute is required to make sure that previous and next are shown on
+		// the webapp only when page number is within limit
+		model.addAttribute("numOfPages", numOfPages);
 		return "employee-page";
 	}
 
 	@GetMapping("/hello")
 	public String helloPage() {
+		//
 		return "hello-world";
 	}
 
--- crud-thymeleaf-demo/src/main/java/com/thymeleaf/crudthymeleafdemo/service/EmployeeService.java
+++ crud-thymeleaf-demo/src/main/java/com/thymeleaf/crudthymeleafdemo/service/EmployeeService.java
@@ -1,38 +1,57 @@
 package com.thymeleaf.crudthymeleafdemo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.thymeleaf.crudthymeleafdemo.entity.Employee;
 import com.thymeleaf.crudthymeleafdemo.repo.EmployeeRepo;
+import com.thymeleaf.crudthymeleafdemo.repo.PageAndSortSupport;
 
 @Service
 public class EmployeeService {
 	// field injection of repo or dao object
 
 	@Autowired
-	private EmployeeRepo employeeRepoDao;
+	private EmployeeRepo employeeRepo;
+
+	@Autowired
+	private PageAndSortSupport repoWitPage;
 
 	@Transactional
 	public List<Employee> findAllEmployees() {
-		return employeeRepoDao.findAllEmployees();
+		return employeeRepo.findAllEmployees();
 	}
 
 	@Transactional
 	public Employee findEmployeeUsingID(Integer id) {
-		return employeeRepoDao.findEmployeeUsingID(id);
+		return employeeRepo.findEmployeeUsingID(id);
 	}
 
 	@Transactional
 	public void saveEmployee(Employee employee) {
-		employeeRepoDao.saveEmployee(employee);
+		employeeRepo.saveEmployee(employee);
 	}
 
 	@Transactional
 	public void deleteEmployee(Integer id) {
-		employeeRepoDao.deleteEmployee(id);
+		employeeRepo.deleteEmployee(id);
 	}
+
+	public List<Employee> findAllWithPage(Integer pageNo, Integer pageSize) {
+		Pageable page = PageRequest.of(pageNo, pageSize);
+		Page<Employee> pagedResult = repoWitPage.findAll(page);
+		if (pagedResult.hasContent()) {
+			return pagedResult.getContent();
+		} else {
+			return new ArrayList<Employee>();
+		}
+	}
+
 }
